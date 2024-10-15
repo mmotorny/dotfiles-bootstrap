595126149ca11e3420c6a3fe75a07f7b871c5195
Maksym Motorny <mmotorny@gmail.com>
2024-10-15 03:52:09 -0700

    Remove debug messages

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,18 +4,13 @@ main() {
   # TODO: Check if the script is running on macOS.
 
   # TODO: Check if Homebrew is installed.
-  echo "[1]"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || return
   # TODO: Investigate is .zprofile can be placed under `.config/`.
-  echo "[2]"
   echo >> ~/.zprofile || return
-  echo "[3]"
   # shellcheck disable=SC2016 # Expansion isn't meant to happen in this script.
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile || return
-  echo "[4]"
   eval "$(/opt/homebrew/bin/brew shellenv)" || return
 
-  echo "[5]"
   brew install git gnupg || return
 
   # TODO: Set up an SSH agent.
