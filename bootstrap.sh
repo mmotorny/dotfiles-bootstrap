94495c52c51f8ce80bdbec6f22e19877b480f214
Maksym Motorny <mmotorny@gmail.com>
2024-10-15 03:19:20 -0700

    Set up Homebrew before using it

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,15 +2,19 @@
 
 main() {
   # TODO: Check if the script is running on macOS.
+  
   # TODO: Check if Homebrew is installed.
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" || return
-  brew install git gnupg || return
   # TODO: Investigate is .zprofile can be placed under `.config/`.
   echo >> ~/.zprofile || return
   # shellcheck disable=SC2016 # Expansion isn't meant to happen in this script.
   echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile || return
   eval "$(/opt/homebrew/bin/brew shellenv)" || return
+
+  brew install git gnupg || return
+
   # TODO: Set up an SSH agent.
+
   rm -rf /tmp/dotfiles-macos-workstation || return
   git clone git@github.com:mmotorny/dotfiles-macos-workstation.git /tmp/dotfiles-macos-workstation || return
   find /tmp/dotfiles-macos-workstation -mindepth 1 -maxdepth 1 -exec mv {} ~ \; || return
